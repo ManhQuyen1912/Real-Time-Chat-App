423c0be45ccb0c87daec3c6180f83cd63c3a488a
ManhQuyen1912 <nmquyen1912@gmail.com>
2024-08-05 17:09:06 +0700

    fix consumer file

--- ChatApp/chat/routing.py
+++ ChatApp/chat/routing.py
@@ -1,5 +1,5 @@
 from django.urls import path , include
-from chat.consumers import ChatConsumer
+from chat.consumer import ChatConsumer
 
 # Here, "" is routing to the URL ChatConsumer which 
 # will handle the chat functionality.
