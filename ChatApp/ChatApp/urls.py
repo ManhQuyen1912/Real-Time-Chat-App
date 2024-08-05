6c0a8747faa8a5db2a9a49a42340d8eaf140c45c
ManhQuyen1912 <nmquyen1912@gmail.com>
2024-08-05 14:36:29 +0700

    update LoginPage

--- ChatApp/ChatApp/urls.py
+++ ChatApp/ChatApp/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urtpattern = [
-    path('admin/', admin.site.url),
+    path('admin/', admin.site.urls),
     path("",include("chat.urls")),
 ]
--- ChatApp/chat/urls.py
+++ ChatApp/chat/urls.py
@@ -3,7 +3,7 @@ from chat import views as chat_views
 from django.contrib.auth.views  import LoginView,LogoutView
 
 urlpatterns = [
-    path("", chat_views.chatPage, name="chat-page"),
+    path("", chat_views.ChatPage, name="chat-page"),
     path("auth/login", LoginView.as_view(template_name="chat/LoginPage.html"), name="login-user"),
     path("auth/logout", LogoutView.as_view(), name="logout-user"),
 ]
